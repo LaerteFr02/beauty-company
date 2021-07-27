39ed7e1db0f020ebc5ed1889fdf03230fcc29bf3
LaerteFr02 <laertefr02@gmail.com>
2021-07-26 22:11:45 -0300

    array de produtos, rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/template/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TabelaComponent } from './tabela/tabela.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '', component: LoginComponent },
+  { path: '', component: AppComponent},
+  { path: 'logar', redirectTo: '/not-found'},
   { path: 'login', component: LoginComponent },
   { path: 'tabela', component: TabelaComponent },
 
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,3 +1,4 @@
+import { Produto } from './../models/produtos.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
+
+  produtos: Produto[] = [
+    {
+      id: 1,
+      nome: "Moleton masculino Eko",
+      preco: 200,
+      tipo: "m",
+      imgPath: "../../assets/images/product_1.png"
+    },
+    {
+      id: 2,
+      nome: "Bolsa Luis Vitao",
+      preco: 200,
+      tipo: "a",
+      imgPath: "../../assets/images/product_2.png"
+    },
+    {
+      id: 3,
+      nome: "Moleton femino Lacoste",
+      preco: 200,
+      tipo: "f",
+      imgPath: "../../assets/images/product_3.png"
+    },
+    {
+      id: 4,
+      nome: "Bolsa bagagem viagem",
+      preco: 200,
+      tipo: "a",
+      imgPath: "../../assets/images/product_4.png"
+    },
+    {
+      id: 5,
+      nome: "Sapato social feminino",
+      preco: 200,
+      tipo: "f",
+      imgPath: "../../assets/images/product_5.png"
+    },
+    {
+      id: 6,
+      nome: "Oculos Rayban",
+      preco: 200,
+      tipo: "m",
+      imgPath: "../../assets/images/product_6.png"
+    },
+    {
+      id: 7,
+      nome: "Blusa meio amarela",
+      preco: 200,
+      tipo: "f",
+      imgPath: "../../assets/images/product_7.png"
+    },
+    {
+      id: 8,
+      nome: "Bolsa de mão",
+      preco: 200,
+      tipo: "a",
+      imgPath: "../../assets/images/product_8.png"
+    },
+    {
+      id: 9,
+      nome: "Jaqueta marron",
+      preco: 200,
+      tipo: "f",
+      imgPath: "../../assets/images/product_9.png"
+    },
+    {
+      id: 10,
+      nome: "Blusa social masculina",
+      preco: 200,
+      tipo: "m",
+      imgPath: "../../assets/images/product_10.png"
+    },
+  ]
 
   constructor() { }
 
